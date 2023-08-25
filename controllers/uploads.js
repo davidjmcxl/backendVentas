5eec3ec00e31d74d6227f705093fb942fcf2a6ca
David Moreno <davidjmcxl@gmail.com>
2023-08-24 23:30:37 -0500

    testss

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -62,7 +62,7 @@ const fileUpload = async(req,res=response)=>{
 const retornaImagen=(req,res=response)=>{
     const tipo =req.params.tipo;
     const foto = req.params.foto;
-    const pathImg = path.join(__dirname,`../uploads/${tipo}/${foto}`);
+    const pathImg = path.join(__dirname,`./uploads/${tipo}/${foto}`);
     //imagen por defecto
     if(fs.existsSync(pathImg)){
         
